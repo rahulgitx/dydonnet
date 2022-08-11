dcddbb0a6e797d46d06f045c9e23d2e3975a638b
Rahul Bhardwaj <rahul18bhardwaj.23@gmail.com>
2022-08-11 09:18:26 +0530

    2

--- Incedo_Octavius_Demo_2/Models/KOL_Image.cs
+++ Incedo_Octavius_Demo_2/Models/KOL_Image.cs
@@ -7,6 +7,7 @@ namespace Incedo_Octavius_Demo_2.Models
 {
     public class KOL_Image
     {
+        public int kolID { get; set; }
         public string First_Name { get; set; }
         public string Last_Name { get; set; }
         public string Image_URL { get; set; }
